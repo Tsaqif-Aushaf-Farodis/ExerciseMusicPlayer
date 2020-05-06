c5168e9ca43f9d3936ad89b9cd945bc13f9cd72b
Tsaqif Aushaf Farodis <tsaqifarodis@gmail.com>
2020-05-06 23:47:19 +0700

    Declare ArrayList & ListView in class MainActivity. find ListView by id & call the list using ArrayList

--- app/src/main/java/com/example/exercisemusicplayer/MainActivity.java
+++ app/src/main/java/com/example/exercisemusicplayer/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.exercisemusicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<song> songList;
+    private ListView songView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        songView = findViewById(R.id.song_list);
+        songList = new ArrayList<song>();
     }
 }
