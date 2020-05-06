28447aedf80de327646e46b14e9a35f366d75517
Tsaqif Aushaf Farodis <tsaqifarodis@gmail.com>
2020-05-06 23:22:18 +0700

    Edit class song, device search containing songs

--- app/src/main/java/com/example/exercisemusicplayer/song.java
+++ app/src/main/java/com/example/exercisemusicplayer/song.java
@@ -5,10 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class song extends AppCompatActivity {
+    private long id;
+    private String title;
+    private String artis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_song);
     }
+
+    public song(long songID, String songTitle, String songArtis){
+        id=songID;
+        title=songTitle;
+        artis=songArtis;
+    }
+
+    public long get_ID(){return id;}
+    public String get_Title(){return title;}
+    public String get_Artist(){return artis;}
 }
