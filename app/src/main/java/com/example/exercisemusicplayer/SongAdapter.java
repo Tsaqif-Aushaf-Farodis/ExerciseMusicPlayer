8d3c8cd1e292f78e8363a914a0c6139eca3be4af
Tsaqif Aushaf Farodis <tsaqifarodis@gmail.com>
2020-05-07 22:48:26 +0700

    Create class SongAdapter, declare ArrayList & LayoutInflater

---
+++ app/src/main/java/com/example/exercisemusicplayer/SongAdapter.java
@@ -0,0 +1,34 @@
+package com.example.exercisemusicplayer;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+
+public class SongAdapter extends BaseAdapter {
+    private ArrayList<song> songs;
+    private LayoutInflater songInflater;
+
+    @Override
+    public int getCount() {
+        return songs.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
