c33f56414d0ce15f4267984a283b866e741d8abd
Tsaqif Aushaf Farodis <tsaqifarodis@gmail.com>
2020-05-07 22:56:21 +0700

    Update implement method getView

--- app/src/main/java/com/example/exercisemusicplayer/SongAdapter.java
+++ app/src/main/java/com/example/exercisemusicplayer/SongAdapter.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -34,6 +36,19 @@ public class SongAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        //map to song layout
+        LinearLayout songLay = (LinearLayout)songInflater.inflate
+                (R.layout.activity_song, parent, false);
+        //get title and artist views
+        TextView songView = (TextView)songLay.findViewById(R.id.song_title);
+        TextView artistView = (TextView)songLay.findViewById(R.id.song_artist);
+        //get song using position
+        song currSong = songs.get(position);
+        //get title and artist strings
+        songView.setText(currSong.get_Title());
+        artistView.setText(currSong.get_Artist());
+        //set position as tag
+        songLay.setTag(position);
+        return songLay;
     }
 }
