9ba5bd947c101043f024330cb555e0d7de43f1f4
Tsaqif Aushaf Farodis <tsaqifarodis@gmail.com>
2020-05-08 22:40:31 +0700

    Create method onCreate in class MusicService

--- app/src/main/java/com/example/exercisemusicplayer/MusicService.java
+++ app/src/main/java/com/example/exercisemusicplayer/MusicService.java
@@ -7,10 +7,26 @@ import android.os.IBinder;
 
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+
 public class MusicService extends Service implements
     MediaPlayer.OnPreparedListener,
     MediaPlayer.OnErrorListener,
     MediaPlayer.OnCompletionListener{
+
+    private MediaPlayer player;
+    private ArrayList<Song> songs;
+    private int songPos; //current position
+
+    public void onCreate(){
+        //create the service
+        super.onCreate();
+        //initialize position
+        songPos=0;
+        //create player
+        player = new MediaPlayer();
+    }
+
     @Override
     public IBinder onBind(Intent intent) {
         return null;
