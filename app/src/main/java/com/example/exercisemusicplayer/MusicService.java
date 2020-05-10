dd134792587af25e9277647c90c1b40a8951564b
Tsaqif Aushaf Farodis <tsaqifarodis@gmail.com>
2020-05-10 18:56:47 +0700

    Create method to apply standard playback control functions

--- app/src/main/java/com/example/exercisemusicplayer/MusicService.java
+++ app/src/main/java/com/example/exercisemusicplayer/MusicService.java
@@ -107,6 +107,30 @@ public class MusicService extends Service implements
         player.prepareAsync();
     }
 
+    public int getPosn(){
+        return player.getCurrentPosition();
+    }
+
+    public int getDur(){
+        return player.getDuration();
+    }
+
+    public boolean isPng(){
+        return player.isPlaying();
+    }
+
+    public void pausePlayer(){
+        player.pause();
+    }
+
+    public void seek(int posn){
+        player.seekTo(posn);
+    }
+
+    public void go(){
+        player.start();
+    }
+
     public void playPrev(){
         songPos--;
         if(songPos<0) songPos=songs.size()-1;
