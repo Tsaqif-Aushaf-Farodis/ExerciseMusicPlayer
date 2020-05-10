2dccd5282e295f76f9c5e8e171ce00fc32ff7d3d
Tsaqif Aushaf Farodis <tsaqifarodis@gmail.com>
2020-05-10 11:21:35 +0700

    Create method to initialize the MediaPlayer

--- app/src/main/java/com/example/exercisemusicplayer/MusicService.java
+++ app/src/main/java/com/example/exercisemusicplayer/MusicService.java
@@ -2,8 +2,10 @@ package com.example.exercisemusicplayer;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.IBinder;
+import android.os.PowerManager;
 
 import androidx.annotation.Nullable;
 
@@ -25,6 +27,17 @@ public class MusicService extends Service implements
         songPos=0;
         //create player
         player = new MediaPlayer();
+        initMusicPlayer();
+    }
+
+    public void initMusicPlayer(){
+        //set player properties
+        player.setWakeMode(getApplicationContext(),
+                PowerManager.PARTIAL_WAKE_LOCK);
+        player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        player.setOnPreparedListener(this);
+        player.setOnCompletionListener(this);
+        player.setOnErrorListener(this);
     }
 
     @Override
