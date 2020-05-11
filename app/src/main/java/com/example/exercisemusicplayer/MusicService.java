0b2ec3df7388b2500bc4d76115c0d59ede24209e
Tsaqif Aushaf Farodis <tsaqifarodis@gmail.com>
2020-05-11 15:21:43 +0700

    Update code

--- app/src/main/java/com/example/exercisemusicplayer/MusicService.java
+++ app/src/main/java/com/example/exercisemusicplayer/MusicService.java
@@ -105,22 +105,22 @@ public class MusicService extends Service implements
     public void onPrepared(MediaPlayer mp) {
         //start playback
         mp.start();
-        Intent notIntent = new Intent(this, MainActivity.class);
-        notIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendInt = PendingIntent.getActivity(this, 0,
-                notIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        Notification.Builder builder = new Notification.Builder(this);
-
-        builder.setContentIntent(pendInt)
-                .setSmallIcon(R.drawable.ic_play_arrow_black_24dp)
-                .setTicker(songTitle)
-                .setOngoing(true)
-                .setContentTitle("Playing")
-                .setContentText(songTitle);
-        Notification not = builder.build();
-
-        startForeground(NOTIFY_ID, not);
+//        Intent notIntent = new Intent(this, MainActivity.class);
+//        notIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//        PendingIntent pendInt = PendingIntent.getActivity(this, 0,
+//                notIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+//
+//        Notification.Builder builder = new Notification.Builder(this);
+//
+//        builder.setContentIntent(pendInt)
+//                .setSmallIcon(R.drawable.ic_play_arrow_black_24dp)
+//                .setTicker(songTitle)
+//                .setOngoing(true)
+//                .setContentTitle("Playing")
+//                .setContentText(songTitle);
+//        Notification not = builder.build();
+//
+//        startForeground(NOTIFY_ID, not);
     }
 
     public void setSong(int songIndex){
